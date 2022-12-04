c9dec09dd120ee972c16a500c390c29309c1df48
toiwilou <toiwilouhassane>
2022-12-04 13:05:38 +0100

    push

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,9 +1,23 @@
 import '../../bootstrap';
 import '../../styles/app/home.css';
 
-for (let i = 0; i < 6; i++) {
-    const btnAdd = document.querySelector('#add-' + i);
+const productContent = document.querySelector('#home-app .main .product-content');
+const cartPrice = document.querySelector('.cart-price');
+const lengthProducts = productContent.dataset.lengthProducts;
+
+for (let i = 0; i < lengthProducts; i++) {
+    const btnAdd = document.querySelector('#btn-add-' + i);
+    const price = parseFloat(btnAdd.dataset.price);
     btnAdd.addEventListener('click', () => {
-        console.log(btnAdd.innerHTML);
+        const cartNummberElement = document.querySelector('.cart-number');
+        let cartNummber = parseInt(cartNummberElement.innerHTML);
+        cartNummber++;
+        cartNummberElement.innerHTML = cartNummber;
+
+        if (cartNummber > 0) {
+            cartNummberElement.removeAttribute('hidden');
+        }
+        const initPrice = parseFloat(cartPrice.innerHTML);
+        cartPrice.innerHTML = price + initPrice;
     });
 }
