381ba55d042ce857a0972e43a588d9f42c94c231
kaissane <kaissanesaidahmed@gmail.com>
2023-01-10 01:48:34 +0000

    search and slideBar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use App\Service\HomeService;
+use Symfony\Component\HttpFoundation\Request;
 use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,22 +13,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     private $homeService;
+    private $productRepository;
     private $productService;
 
     public function __construct(
         HomeService $homeService,
-        ProductService $productService)
+        ProductService $productService,
+        ProductRepository $productRepository)
     {
         $this->homeService = $homeService;
+        $this->productRepository = $productRepository;
         $this->productService = $productService;
     }
 
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        
+        $search = $request->query->get('search');
         return $this->render('app/home/index.html.twig', [
             'categories' => $this->homeService->getCategories(),
             'products' => $this->productService->getProducts(),
+            'products' => $this->productRepository->recherche($search),
+            
         ]);
     }
+    
+    
+    
+    
+    
 }
